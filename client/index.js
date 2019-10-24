13bb43ee7025b219b702aec040944465a714fc7b
Eva E-P <eva.emendoerfer@gmail.com>
2019-10-24 12:47:07 -0400

    clean code

--- client/index.js
+++ client/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store/index";
 import Main from "./components/Main";
-import Home from "./components/Home";
 
 ReactDOM.render(
   <Provider store={store}>
